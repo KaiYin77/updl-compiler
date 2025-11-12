ea8b39b3531ecfe1d7d91b57ecce4e47587c16df
kai-yin hung <kaiyin0208.ee07@nycu.edu.tw>
2025-11-12 15:24:23 +0800

    [KWS] - add flatten layer

---
+++ examples/keyword_spotting/uph5/kws_test_layers_fp32_flatten.h
@@ -0,0 +1,12 @@
+#ifndef KWS_TEST_LAYERS_FP32_FLATTEN_H
+#define KWS_TEST_LAYERS_FP32_FLATTEN_H
+
+#include "kws_test_input_data_fp32.h"
+#include <stddef.h>
+
+#define kNumKwsTestInputs 20
+#define kKwsFlattenFeatureSize 64
+
+extern const float g_kws_flatten_fp32[kNumKwsTestInputs][kKwsFlattenFeatureSize];
+
+#endif // KWS_TEST_LAYERS_FP32_FLATTEN_H
