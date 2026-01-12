88bf52610ac3c697812aed9f9e99df435a0750ae
kai-yin hung <kaiyin0208.ee07@nycu.edu.tw>
2026-01-12 15:58:43 +0800

    [IC] - Add test layers

---
+++ examples/image_classification/uph5/ic_test_layers_fp32_batch_normalization_2.h
@@ -0,0 +1,11 @@
+#ifndef IC_TEST_LAYERS_FP32_BATCH_NORMALIZATION_2_H
+#define IC_TEST_LAYERS_FP32_BATCH_NORMALIZATION_2_H
+
+#include <stddef.h>
+
+#define kNumIcTestInputs 5
+#define kIcBatchNormalization2FeatureSize 16384
+
+extern const float g_ic_batch_normalization_2_fp32[kNumIcTestInputs][kIcBatchNormalization2FeatureSize];
+
+#endif // IC_TEST_LAYERS_FP32_BATCH_NORMALIZATION_2_H
